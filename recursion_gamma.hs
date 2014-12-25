dbfc215a22dd6f6e64e3eee84482dc3fe35922f1
shigemk2 <i.am.shige@gmail.com>
2014-12-26 00:52:11 +0900

    再帰

---
+++ recursion_gamma.hs
@@ -0,0 +1,31 @@
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' _ [] = []
+filter' f (x:xs)
+    | f x       = x : filter' f xs
+    | otherwise = filter' f xs
+
+flip' f xs g = f g xs
+
+foldl' _ x [] = x
+foldl' f x (y:ys) = foldl' f (f x y) ys
+
+foldr' _ x [] = x
+foldr' f x (y:ys) = f y $ foldr' f x ys
+
+main = do
+    print $ map (* 2) [1..5]
+    print $ filter (< 5) [1..9]
+    print $ flip map [1..5] (* 2)
+    print $ foldl (+) 0 [1..100]
+    print $ foldl (-) 0 [1..5]
+    print $ foldr (-) 0 [1..5]
+    print $ "----------------"
+    print $ map' (* 2) [1..5]
+    print $ filter' (< 5) [1..9]
+    print $ flip' map' [1..5] (* 2)
+    print $ flip' filter' [1..5] (< 2)
+    print $ foldl' (+) 0 [1..100]
+    print $ foldl' (-) 0 [1..5]
+    print $ foldr' (-) 0 [1..5]
