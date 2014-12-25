4e936e9720d2711197a387600b5b87aa152b2c11
shigemk2 <i.am.shige@gmail.com>
2014-12-25 23:48:54 +0900

    再帰 ベース

---
+++ recursion_alpha.hs
@@ -0,0 +1,13 @@
+main = do
+    print $ map (* 2) [1..5]
+    print $ filter (< 5) [1..9]
+    print $ flip map [1..5] (* 2)
+    print $ foldl (+) 0 [1..100]
+    print $ foldl (-) 0 [1..5]
+    print $ foldr (-) 0 [1..5]
+    -- print $ map' (* 2) [1..5]
+    -- print $ filter' (< 5) [1..9]
+    -- print $ flip' map' [1..5] (* 2)
+    -- print $ foldl' (+) 0 [1..100]
+    -- print $ foldl' (-) 0 [1..5]
+    -- print $ foldr' (-) 0 [1..5]
