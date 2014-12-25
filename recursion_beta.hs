9bd35b204a21b7a4a8e068b0e44f4d9f066d8895
shigemk2 <i.am.shige@gmail.com>
2014-12-25 23:56:23 +0900

    filter 再帰

--- recursion_beta.hs
+++ recursion_beta.hs
@@ -1,6 +1,11 @@
 map' _ [] = []
 map' f (x:xs) = (f x) : (map' f xs)
 
+filter' _ [] = []
+filter' f (x:xs)
+    | (f x) = x : (filter' f xs)
+    | otherwise = (filter' f xs)
+
 main = do
     print $ map (* 2) [1..5]
     print $ filter (< 5) [1..9]
@@ -8,8 +13,9 @@ main = do
     print $ foldl (+) 0 [1..100]
     print $ foldl (-) 0 [1..5]
     print $ foldr (-) 0 [1..5]
+    print $ "----------------"
     print $ map' (* 2) [1..5]
-    -- print $ filter' (< 5) [1..9]
+    print $ filter' (< 5) [1..9]
     -- print $ flip' map' [1..5] (* 2)
     -- print $ foldl' (+) 0 [1..100]
     -- print $ foldl' (-) 0 [1..5]
