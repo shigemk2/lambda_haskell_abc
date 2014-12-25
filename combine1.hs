c52ff6551c1ddec644a78126d386bd4035079892
shigemk2 <i.am.shige@gmail.com>
2014-12-25 13:42:45 +0900

    combineを、引数1つずつに分割してネストさせたラムダ式で書き換える

---
+++ combine1.hs
@@ -0,0 +1,14 @@
+combine a b c = a:b:[c]
+combine' a b = \c -> a:b:[c]
+combine'' a = \b -> \c -> a:b:[c]
+combine''' = \a -> \b -> \c -> a:b:[c]
+
+main = do
+    let a = combine 1
+        b = a 2
+        c = b 3
+    print c
+    print $ combine 'a' 'b' 'c'
+    print $ combine' 'a' 'b' 'c'
+    print $ combine'' 'a' 'b' 'c'
+    print $ combine''' 'a' 'b' 'c'
