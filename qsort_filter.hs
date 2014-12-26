21fd26a75f19e39e91c8881a88d012cc5b5ebe2f
shigemk2 <i.am.shige@gmail.com>
2014-12-26 22:55:01 +0900

    クイックソートをfiterで

---
+++ qsort_filter.hs
@@ -0,0 +1,8 @@
+qsort []     = []
+qsort (n:xs) = qsort lt ++ [n] ++ qsort gteq
+    where
+        lt   = filter (< n) xs
+        gteq = filter (>= n) xs
+
+main = do
+    print $ qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
