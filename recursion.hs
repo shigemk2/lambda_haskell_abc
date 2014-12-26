20649132d71b4d3ac545e1f0478fb7089fc31db9
shigemk2 <i.am.shige@gmail.com>
2014-12-26 22:45:26 +0900

    foldlでreverse maximum minimum

---
+++ recursion.hs
@@ -0,0 +1,12 @@
+reverse' xs = foldl (flip (:)) [] xs
+maximum' (x:xs) = foldl max x (x:xs)
+minimum' (x:xs) = foldl min x (x:xs)
+
+main = do
+    let src = [-5..5]
+    print $ foldl (flip (:)) [] src
+    print $ foldl max (head src) src
+    print $ foldl min (head src) src
+    print $ reverse' src
+    print $ maximum' src
+    print $ minimum' src
