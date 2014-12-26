17b981db0958bd3ab7f89c9dd2277aef61ea848e
shigemk2 <i.am.shige@gmail.com>
2014-12-26 23:18:27 +0900

    foldrでreverse

--- recursion.hs
+++ recursion.hs
@@ -1,4 +1,5 @@
 reverse' xs = foldl (flip (:)) [] xs
+reverse'' xs = foldr (:) [] xs
 maximum' (x:xs) = foldl max x (x:xs)
 minimum' (x:xs) = foldl min x (x:xs)
 
@@ -8,5 +9,6 @@ main = do
     print $ foldl max (head src) src
     print $ foldl min (head src) src
     print $ reverse' src
+    print $ reverse'' src
     print $ maximum' src
     print $ minimum' src
