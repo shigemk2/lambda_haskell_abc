28ba7e551a60be698e79848a9c1bb8619fbbc1bd
shigemk2 <i.am.shige@gmail.com>
2015-01-17 17:50:44 +0900

    Yコンビネーター補足

--- yconbinator.hs
+++ yconbinator.hs
@@ -1,6 +1,22 @@
-z x = x (z x)
+-- Yコンビネーターに渡す引数は2つ
+-- Yコンビネーター経由で自分自身を受け取っている
+y f a = f (y f) a
 
 main = do
-    print $ flip z [1..100] $ \f (x:xs) -> case xs of
-        [] -> x
-        _  -> x + f xs
+    -- print $ flip z [1..100] $ \f (x:xs) -> case xs of
+    --     [] -> x
+    --     _  -> x + f xs
+    -- flipを使わない
+    print $ y
+            (\sum (x:xs) ->
+                 case xs of
+                   [] -> x
+                   _  -> x + sum xs)
+            [1..100]
+    -- mapは使えない
+    -- print $ map
+    --         (\f (x:xs) ->
+    --              case xs of
+    --                [] -> x
+    --                _  -> x + f xs)
+    --         [1..100]
